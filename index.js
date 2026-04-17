7c3df2fcf737d27847849116a27d3d7b11054b6e
SmiteshMagdum <smiteshmagdum@gmail.com>
2026-04-17 19:05:08 +0530

    depoloying frontend and backend at same server

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
-app.use(cors())
 app.use(express.json())
+
+app.get("/",function(req, res){
+  res.sendFile(__dirname + "/public/index.htm")
+})
 
 app.post("/sum", function(req,res){
   const a = parseInt(req.body.a)
   const b = parseInt(req.body.b)
-
+   
 
   res.json({
-    answer: a + b
+    answer: a + b,
+    
   })
 })
 

e2de72d4dfe62831f8f1099b04626f171184493b
SmiteshMagdum <smiteshmagdum@gmail.com>
2026-04-17 09:40:00 +0530

    middlewares

--- index.js
+++ index.js
@@ -1,51 +1,50 @@
 const express = require("express");
+
 const app = express();
+let requestCounter = 0
+function middleWare(req, res, next){
+  requestCounter = requestCounter + 1 
+  console.log(`request Count is ${requestCounter}`);
+  
+   next()
+   
+}
 
+app.use(middleWare)
 
-
-app.get("/add/:a/:b", function (req, res) {
-  const a = parseInt(req.params.a);
-  const b = parseInt(req.params.b);
-
-  const sum = a + b;
-
-  res.send({
-    result: sum
-  });
-});
-app.get("/divide/:a/:b", function (req, res) {
-  const a = parseInt(req.params.a);
-  const b = parseInt(req.params.b);
-
-  const divide = a / b;
-
-  res.send({
-    result: divide
-  });
-});
-app.get("/multiply", function (req, res) {
-  const a = parseInt(req.params.a);
-  const b = parseInt(req.params.b);
-
-  const multiply = a * b;
-
-  res.send({
-    result: multiply
-  });
-});
-app.get("/sub", function (req, res) {
-  const a = parseInt(req.params.a);
-  const b = parseInt(req.params.b);
-
-  const subtract = a - b;
-
-  res.send({
-    result: subtract
-  });
+app.get("/sum", function(req, res) {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+ 
+    res.json({
+        ans: a + b
+    })
+    
 });
 
+app.get("/multiply", function(req, res) {
+    const a = req.query.a;
+    const b = req.query.b;
+    res.json({
+        ans: a * b
+    })
+});
 
+app.get("/divide", function(req, res) {
+    const a = req.query.a;
+    const b = req.query.b;
+    res.json({
+        ans: a / b
+    })
 
+});
+
+app.get("/subtract", function(req, res) {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    res.json({
+        ans: a - b
+    })
+});
 
 app.listen(3000);
-   
